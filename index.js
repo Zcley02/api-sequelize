f17ff95fe8ad60ff592531eb4b175124882631bc
cleymer <cleymer.zavala8807@est.uca.edu.ni>
2022-08-10 17:31:36 -0600

    First sequelize api

---
+++ index.js
@@ -0,0 +1,24 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const morgan = require('morgan');
+
+const user = require('./routes/user');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome" });
+});
+
+app.use('/user', user);
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+});
